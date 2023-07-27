e0f3622688da1adb11978b437af200a49cb1abad
dcjacobswri <darryl.jacob@swri.org>
2023-07-26 21:58:49 -0500

    added script to get urls to 10Q

---
+++ Fundamentals/stocks_singleTicker.py
@@ -0,0 +1,28 @@
+import requests
+from sec_edgar_api import EdgarClient
+
+headers = {
+    'User-Agent': 'admin admin@moneybagz.com'
+}
+
+CIK = "12927"
+CIK = '0'*(10-len(CIK))+CIK
+
+""" url = "https://data.sec.gov/api/xbrl/companyfacts/CIK{}.json?from=2000&last=2023".format(CIK)
+response = requests.get(url, headers=headers)
+response = response.json()
+ """
+
+statements = {}
+url = "https://www.sec.gov/Archives/edgar/data/{}/{}/{}"
+edgar = EdgarClient( user_agent='admin admin@moneybagz.com' )
+all_filings = edgar.get_submissions(CIK)['filings']['recent']
+for idx, accessionNumber in enumerate( all_filings['accessionNumber'] ):
+    if all_filings['form'][idx] == '10-Q':
+        primaryDocument = all_filings['primaryDocument'][idx]
+        if primaryDocument == '':
+            primaryDocument = accessionNumber + '.txt'
+        statements[ all_filings['reportDate'][idx] ] = url.format( CIK, accessionNumber.replace('-', ''), primaryDocument )
+
+statement = list(statements.values())[-1]
+response = requests.get( statement, headers=headers )

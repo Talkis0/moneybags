8975b940e3715f4a7f3095f6397b7d960ab57cc1
dcjacobswri <darryl.jacob@swri.org>
2023-07-31 22:35:14 -0500

    combined dfs

--- Fundamentals/stocks_singleTicker.py
+++ Fundamentals/stocks_singleTicker.py
@@ -2,6 +2,7 @@ import requests
 from sec_edgar_api import EdgarClient
 import datetime
 import pandas as pd
+import numpy as np
 
 headers = {
     'User-Agent': 'admin admin@moneybagz.com'
@@ -15,18 +16,39 @@ response = requests.get(url, headers=headers)
 response = response.json()
 # Dates Assets Liabilities EarningsPerShareDiluted WeightedAverageNumberOfDilutedSharesOutstanding
 assets_raw = response['facts']['us-gaap']['Assets']['units']['USD']
-liabilities_raw = response['facts']['us-gaap']['Liabilities']['units']['USD']
+se_raw = response['facts']['us-gaap']['StockholdersEquity']['units']['USD']
 eps_diluted_raw = response['facts']['us-gaap']['EarningsPerShareDiluted']['units']['USD/shares']
 n_shares_diluted_raw = response['facts']['us-gaap']['WeightedAverageNumberOfDilutedSharesOutstanding']['units']['shares']
 
 # sort by date and filter for 10-Q/10-K
-assets_raw = sorted( filter( lambda a: ( a['form'] == '10-Q' ) or ( a['form'] == '10K' ), assets_raw ), key=lambda k: datetime.datetime.strptime( k['filed'], '%Y-%m-%d' ) )
-liabilities_raw = sorted( filter( lambda a: ( a['form'] == '10-Q' ) or ( a['form'] == '10K' ), liabilities_raw ), key=lambda k: datetime.datetime.strptime( k['filed'], '%Y-%m-%d' ) )
-eps_diluted_raw = sorted( filter( lambda a: ( a['form'] == '10-Q' ) or ( a['form'] == '10K' ), eps_diluted_raw ), key=lambda k: datetime.datetime.strptime( k['filed'], '%Y-%m-%d' ) )
-n_shares_diluted_raw = sorted( filter( lambda a: ( a['form'] == '10-Q' ) or ( a['form'] == '10K' ), n_shares_diluted_raw ), key=lambda k: datetime.datetime.strptime( k['filed'], '%Y-%m-%d' ) )
+assets_raw = sorted( assets_raw, key=lambda k: datetime.datetime.strptime( k['filed'], '%Y-%m-%d' ) )
+se_raw = sorted( se_raw, key=lambda k: datetime.datetime.strptime( k['filed'], '%Y-%m-%d' ) )
+eps_diluted_raw = sorted( eps_diluted_raw, key=lambda k: datetime.datetime.strptime( k['filed'], '%Y-%m-%d' ) )
+n_shares_diluted_raw = sorted( n_shares_diluted_raw, key=lambda k: datetime.datetime.strptime( k['filed'], '%Y-%m-%d' ) )
 
-# Convert to pandas series
-assets_df = pd.Series( [ a['val'] for a in assets_raw ], index=pd.to_datetime( [ a['filed'] for a in assets_raw ] ) )
-liabilities_df = pd.Series( [ a['val'] for a in liabilities_raw ], index=pd.to_datetime( [ a['filed'] for a in liabilities_raw ] ) )
-eps_diluted_df = pd.Series( [ a['val'] for a in eps_diluted_raw ], index=pd.to_datetime( [ a['filed'] for a in eps_diluted_raw ] ) )
-n_shares_diluted_df = pd.Series( [ a['val'] for a in n_shares_diluted_raw ], index=pd.to_datetime( [ a['filed'] for a in n_shares_diluted_raw ] ) )
+# Convert to pandas series and remove duplicate rows
+def aggregate_duplicate_times( rows ):
+    best_form = '10-Q'
+    best_val = np.inf
+    for r in rows:
+        if ( r[0] == '10-K' ) and ( best_val > r[1] ):
+            best_form = '10-K'
+            best_val = r[1]
+        elif ( r[0] == '10-Q' ) and ( best_form == '10-Q' ) and ( best_val > r[1] ):
+            best_val = r[1]
+    return best_val
+
+assets_df = pd.Series( [ ( a['form'], a['val'] ) for a in assets_raw ], index=pd.to_datetime( [ a['filed'] for a in assets_raw ] ) )
+assets_df = assets_df.groupby( assets_df.index ).agg( aggregate_duplicate_times )
+se_df = pd.Series( [ ( a['form'], a['val'] ) for a in se_raw ], index=pd.to_datetime( [ a['filed'] for a in se_raw ] ) )
+se_df = se_df.groupby( se_df.index ).agg( aggregate_duplicate_times )
+eps_diluted_df = pd.Series( [ ( a['form'], a['val'] ) for a in eps_diluted_raw ], index=pd.to_datetime( [ a['filed'] for a in eps_diluted_raw ] ) )
+eps_diluted_df = eps_diluted_df.groupby( eps_diluted_df.index ).agg( aggregate_duplicate_times )
+n_shares_diluted_df = pd.Series( [ ( a['form'], a['val'] ) for a in n_shares_diluted_raw ], index=pd.to_datetime( [ a['filed'] for a in n_shares_diluted_raw ] ) )
+n_shares_diluted_df = n_shares_diluted_df.groupby( n_shares_diluted_df.index ).agg( aggregate_duplicate_times )
+
+fundamentals_df = pd.DataFrame({})
+fundamentals_df['assets'] = assets_df
+fundamentals_df['stockholders_equity'] = se_df
+fundamentals_df['eps_diluted'] = eps_diluted_df
+fundamentals_df['n_shares_diluted'] = n_shares_diluted_df

35533769822cc9387180cb57120e8eb00cdf7b77
dcjacobswri <darryl.jacob@swri.org>
2023-08-03 20:33:01 -0500

    added accuracy, precision and recall stats

--- Model/model.py
+++ Model/model.py
@@ -4,21 +4,33 @@ import pandas as pd
 import matplotlib.pyplot as plot
 import seaborn as sns
 import numpy as np
+from sklearn.metrics import precision_score, recall_score, accuracy_score
 from scipy.stats import probplot, norm
 from scipy.stats import kstest
+from sklearn.utils import shuffle
 sns.set_style('darkgrid')
 data = pd.read_csv('../Stocks_daily/BA.csv')
 data.index = pd.to_datetime( data.index )
 data['Next_Close'] = data['Close'].shift(1) 
+data['Buy'] = np.where( data['Next_Close'] > data['Close'], 1, 0 )
 data = data.dropna()
 data = data.sort_index( ascending=True )
 input_vars = ['Open', 'High', 'Low', 'Close']
 output_var = 'Next_Close'
 X = data[ input_vars ]
 y = data[ output_var ]
-X_train, X_test, y_train, y_test = train_test_split( X, y, shuffle=False, train_size=0.2 )
+X_train, X_test, y_train, y_test, idx_train, idx_test = train_test_split( X, y, data.index, shuffle=False, train_size=0.2 )
+X_train, y_train = shuffle( X_train, y_train )
 regr = LinearRegression().fit( X_train, y_train )
 y_hat = regr.predict( X )
+data['Next_Close.estimate'] = y_hat
+data['Buy.estimate'] = np.where( data['Next_Close.estimate'] > data['Close'], 1, 0 )
+test_accuracy = accuracy_score( data['Buy'].loc[idx_test], data['Buy.estimate'].loc[idx_test] )
+test_precision = precision_score( data['Buy'].loc[idx_test], data['Buy.estimate'].loc[idx_test] )
+test_recall = recall_score( data['Buy'].loc[idx_test], data['Buy.estimate'].loc[idx_test] )
+print( 'Test Accuracy:', test_accuracy )
+print( 'Test Precision:', test_precision )
+print( 'Test Recall:', test_recall )
 print( 'R2:', regr.score( X, y ) )
 residual = y - y_hat
 residual_mean = np.mean(residual)

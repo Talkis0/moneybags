7024d0b130d2aaaff0daf2a47197d7e0a9deae34
dcjacobswri <darryl.jacob@swri.org>
2023-08-03 19:36:25 -0500

    residual statistics

--- Model/model.py
+++ Model/model.py
@@ -3,6 +3,9 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plot
 import seaborn as sns
+import numpy as np
+from scipy.stats import probplot, norm
+from scipy.stats import kstest
 sns.set_style('darkgrid')
 data = pd.read_csv('../Stocks_daily/BA.csv')
 data.index = pd.to_datetime( data.index )
@@ -15,11 +18,37 @@ y = data[ output_var ]
 X_train, X_test, y_train, y_test = train_test_split( X, y, train_size=0.8 )
 regr = LinearRegression().fit( X_train, y_train )
 y_hat = regr.predict( X )
-plot.figure()
-plot.plot( data.index, y, 'b-', label='True' )
-plot.plot( data.index, y_hat, 'r-', label='Estimate' )
-plot.ylabel( 'Closing Price (USD)' )
-plot.xlabel( 'Time' )
+residual = y - y_hat
+residual_mean = np.mean(residual)
+residual_std = np.std(residual)
+print( kstest( ( residual - residual_mean ) / residual_std, norm.cdf, N=10000, method='exact' ) )
+# Plot residual statistics
+plot.figure( figsize=(10,8) )
+plot.subplot(2, 2, 1)
+plot.hist(residual, density=True)
+t = np.linspace( residual_mean - 5.*residual_std, residual_mean + 5.*residual_std )
+plot.plot( t, norm.pdf(t, residual_mean, residual_std), 'r-', label='Normal PDF' )
+plot.ylabel('$P(\epsilon)$')
+plot.xlabel('$\epsilon = \hat{{y}} - y$')
+plot.title( 'Residual Distribution' )
 plot.legend()
+plot.subplot(2, 2, 2)
+plot.plot( y_hat, np.zeros( (y_hat.shape[0],) ), 'r-', label='$\epsilon = 0$' )
+plot.scatter( y_hat, residual )
+plot.xlabel('$\hat{{y}}$')
+plot.ylabel('$\epsilon = \hat{{y}} - y$')
+plot.title( 'Residual vs. Predicted' )
+plot.legend()
+plot.subplot(2, 2, 3)
+probplot( ( residual - residual_mean ) / residual_std, dist='norm', plot=plot)
+plot.title('Q-Q Plot (against Normal Distribution)')
+plot.subplot(2, 2, 4)
+plot.scatter( y_hat, y )
+plot.plot(sorted( y ), sorted( y ), 'r-', label='$y = \hat{{y}}$')
+plot.xlabel('$\hat{{y}}$')
+plot.ylabel('$y$')
+plot.title( 'True vs. Predicted' )
+plot.legend()
+plot.suptitle('E$[\epsilon] = ${:.5f}; Var$[\epsilon] = ${:.5f};'.format( residual_mean, residual_std**2. ))
 plot.tight_layout()
 plot.show()

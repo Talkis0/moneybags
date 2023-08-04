585dad564f441d87614d3768be92378d067fb7af
dcjacobswri <darryl.jacob@swri.org>
2023-08-03 20:38:41 -0500

    Added ROC curve for classifying buy events

--- Model/model.py
+++ Model/model.py
@@ -1,10 +1,10 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plot
 import seaborn as sns
 import numpy as np
-from sklearn.metrics import precision_score, recall_score, accuracy_score
+from sklearn.metrics import precision_score, recall_score, accuracy_score, roc_auc_score, roc_curve
 from scipy.stats import probplot, norm
 from scipy.stats import kstest
 from sklearn.utils import shuffle
@@ -16,56 +16,41 @@ data['Buy'] = np.where( data['Next_Close'] > data['Close'], 1, 0 )
 data = data.dropna()
 data = data.sort_index( ascending=True )
 input_vars = ['Open', 'High', 'Low', 'Close']
-output_var = 'Next_Close'
+output_var = 'Buy'
 X = data[ input_vars ]
 y = data[ output_var ]
-X_train, X_test, y_train, y_test, idx_train, idx_test = train_test_split( X, y, data.index, shuffle=False, train_size=0.2 )
+X_train, X_test, y_train, y_test, idx_train, idx_test = train_test_split( X, y, data.index, shuffle=False, train_size=0.4 )
 X_train, y_train = shuffle( X_train, y_train )
-regr = LinearRegression().fit( X_train, y_train )
+regr = LogisticRegression().fit( X_train, y_train )
 y_hat = regr.predict( X )
-data['Next_Close.estimate'] = y_hat
-data['Buy.estimate'] = np.where( data['Next_Close.estimate'] > data['Close'], 1, 0 )
+data['Buy.estimate'] = y_hat
 test_accuracy = accuracy_score( data['Buy'].loc[idx_test], data['Buy.estimate'].loc[idx_test] )
 test_precision = precision_score( data['Buy'].loc[idx_test], data['Buy.estimate'].loc[idx_test] )
 test_recall = recall_score( data['Buy'].loc[idx_test], data['Buy.estimate'].loc[idx_test] )
 print( 'Test Accuracy:', test_accuracy )
 print( 'Test Precision:', test_precision )
 print( 'Test Recall:', test_recall )
-print( 'R2:', regr.score( X, y ) )
-residual = y - y_hat
-residual_mean = np.mean(residual)
-residual_std = np.std(residual)
-print( 'KSTest:', kstest( ( residual - residual_mean ) / residual_std, norm.cdf, N=10000, method='exact' ) )
-# Plot residual statistics
-plot.figure( figsize=(10,8) )
-plot.subplot(3, 2, 1)
-plot.hist(residual, color='blue', density=True)
-t = np.linspace( residual_mean - 5.*residual_std, residual_mean + 5.*residual_std )
-plot.plot( t, norm.pdf(t, residual_mean, residual_std), 'r-', label='Normal PDF' )
-plot.ylabel('$P(\epsilon)$')
-plot.xlabel('$\epsilon = \hat{{y}} - y$')
-plot.title( 'Residual Distribution' )
+# Plot residual statistics# predict probabilities
+ns_probs = [0 for _ in range(len(y_test))]
+lr_probs = regr.predict_proba(X_test)
+# keep probabilities for the positive outcome only
+lr_probs = lr_probs[:, 1]
+# calculate scores
+ns_auc = roc_auc_score(y_test, ns_probs)
+lr_auc = roc_auc_score(y_test, lr_probs)
+# summarize scores
+print('No Skill: ROC AUC=%.3f' % (ns_auc))
+print('Logistic: ROC AUC=%.3f' % (lr_auc))
+# calculate roc curves
+ns_fpr, ns_tpr, _ = roc_curve(y_test, ns_probs)
+lr_fpr, lr_tpr, _ = roc_curve(y_test, lr_probs)
+# plot the roc curve for the model
+plot.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+plot.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+# axis labels
+plot.xlabel('False Positive Rate')
+plot.ylabel('True Positive Rate')
+# show the legend
 plot.legend()
-plot.subplot(3, 2, 2)
-plot.plot( y_hat, np.zeros( (y_hat.shape[0],) ), 'r-', label='$\epsilon = 0$' )
-plot.scatter( y_hat, residual, c='b' )
-plot.xlabel('$\hat{{y}}$')
-plot.ylabel('$\epsilon = \hat{{y}} - y$')
-plot.title( 'Residual vs. Predicted' )
-plot.legend()
-plot.subplot(3, 2, 3)
-probplot( ( residual - residual_mean ) / residual_std, dist='norm', plot=plot)
-plot.title('Q-Q Plot (against Normal Distribution)')
-plot.subplot(3, 2, 4)
-plot.scatter( y_hat, y, c='b' )
-plot.plot(sorted( y ), sorted( y ), 'r-', label='$y = \hat{{y}}$')
-plot.xlabel('$\hat{{y}}$')
-plot.ylabel('$y$')
-plot.title( 'True vs. Predicted' )
-plot.legend()
-plot.subplot(3, 2, 5)
-plot.plot( data.index, y, 'r-', label='True' )
-plot.plot( data.index, y_hat, 'b-', label='Estimate' )
-plot.suptitle('E$[\epsilon] = ${:.5f}; Var$[\epsilon] = ${:.5f};'.format( residual_mean, residual_std**2. ))
-plot.tight_layout()
+# show the plot
 plot.show()

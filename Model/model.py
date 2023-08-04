3263fdf303158f5efc9b3027215504add69def54
dcjacobswri <darryl.jacob@swri.org>
2023-08-03 20:43:26 -0500

    Trimming

--- Model/model.py
+++ Model/model.py
@@ -1,12 +1,10 @@
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plot
 import seaborn as sns
 import numpy as np
 from sklearn.metrics import precision_score, recall_score, accuracy_score, roc_auc_score, roc_curve
-from scipy.stats import probplot, norm
-from scipy.stats import kstest
 from sklearn.utils import shuffle
 sns.set_style('darkgrid')
 data = pd.read_csv('../Stocks_daily/BA.csv')
@@ -15,7 +13,6 @@ data.sort_index( inplace=True )
 data['Next_Close'] = data['Close'].shift(-1) 
 data['Buy'] = np.where( data['Next_Close'] > data['Close'], 1, 0 )
 data = data.dropna()
-data = data.sort_index( ascending=True )
 input_vars = ['Open', 'High', 'Low', 'Close']
 output_var = 'Buy'
 X = data[ input_vars ]

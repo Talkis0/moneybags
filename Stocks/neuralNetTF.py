faf3a2865e5ee9ba8a7e507f0c8e2d6a5047096b
Tristan Alkis <tristan_alkis@utexas.edu>
2023-08-05 15:08:41 -0500

    debugg

--- Stocks/neuralNetTF.py
+++ Stocks/neuralNetTF.py
@@ -25,23 +25,30 @@ endTrain_index = len(data_df) + 365*10
 
 # Drop rows with NaN values in the target columns due to shifting
 # X = X.dropna()
-y_1_week = y_1_week.dropna()
-y_2_weeks = y_2_weeks.dropna()
-y_3_weeks = y_3_weeks.dropna()
-
+print('length before changes\n',len(X),len(y_1_week),len(y_2_weeks),len(y_3_weeks))
+X = X.iloc[21:]
+y_1_week = y_1_week.iloc[14:-7]
+y_2_weeks = y_2_weeks.iloc[7:-14]
+y_3_weeks = y_3_weeks.iloc[:-21]
+# y_1_week = y_1_week.dropna()
+# y_2_weeks = y_2_weeks.dropna()
+# y_3_weeks = y_3_weeks.dropna()
+print('lengths of X, y_1_week, y_2_week, y_3_week\n', len(X),len(y_1_week), len(y_2_weeks), len(y_3_weeks))
+print('lengths of X, y_1_week, y_2_week, y_3_week\n', X.index,y_1_week.index, y_2_weeks.index, y_3_weeks.index)
 # Combine the target values into a single DataFrame
-y = pd.DataFrame({'Close_1_Week': y_1_week, 'Close_2_Weeks': y_2_weeks, 'Close_3_Weeks': y_3_weeks})
+y = pd.DataFrame({'Close_1_Week': y_1_week.values, 'Close_2_Weeks': y_2_weeks.values, 'Close_3_Weeks': y_3_weeks.values}, index=None)
 
 # Step 3: Normalize Data
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
 y_scaled = scaler.fit_transform(y)
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(y_2_weeks)
+print('length of X_scaled and y_scaled\n', len(X_scaled), len(y_scaled))
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+#     print(y_2_weeks)
 #     print(len(y_scaled),len(X_scaled))
     # print(y_scaled)
 # Split data into training and validation sets
-X_train, X_val, y_train, y_val = train_test_split(X_scaled, y_scaled, test_size=0.4, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_scaled, y_scaled, test_size=0.4, random_state=42, shuffle = False)
 
 # Step 4: Create and Train the Model
 model = tf.keras.Sequential([
@@ -62,6 +69,7 @@ y_pred = scaler.inverse_transform(y_pred_scaled)
 y_val_actual = scaler.inverse_transform(y_val)
 
 # Step 7: Plot the Results
+print(np.average(np.square([y_val_actual-y_pred])))
 for i in range(3):
     plt.figure(figsize=(10, 6))
     plt.plot(y_val_actual[:, i], label=f'Actual {i+1}-Week')

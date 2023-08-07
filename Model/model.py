a313a03118ef0f9b73ec97b934ff4bddd09ee8e8
dcjacobswri <darryl.jacob@swri.org>
2023-08-07 18:30:25 -0500

    82 acc 98 recall 75 precision

--- Model/model.py
+++ Model/model.py
@@ -4,19 +4,19 @@ import seaborn as sns
 import matplotlib.pyplot as plot
 from sklearn.metrics import precision_score, recall_score, accuracy_score, roc_auc_score, roc_curve
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegressionCV, LinearRegression, LassoCV
+from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 from scipy.stats import probplot, norm
+from sklearn.preprocessing import SplineTransformer
 sns.set_style('darkgrid')
 # Data Cleaning
 data = pd.read_csv('../Stocks_daily/BA.csv', index_col=0)
 data.index = pd.to_datetime( data.index, format='%Y-%m-%d' )
 data.sort_index( inplace=True, ascending=True )
-data['Close.lag1'] = data['Close'].shift(1)
-data['Close.lag2'] = data['Close'].shift(2)
-data['Close.lag4'] = data['Close'].shift(4)
-data['Close.lag6'] = data['Close'].shift(6)
-data['Close.lag8'] = data['Close'].shift(8)
+data['Close.lag1'] = data['Close'].shift(1) / data['Close'] - 1.
+data['Close.lag2'] = data['Close'].shift(2) / data['Close'] - 1.
+data['Close.lag4'] = data['Close'].shift(4) / data['Close'] - 1.
+data['Close.lag8'] = data['Close'].shift(8) / data['Close'] - 1.
 data['Close.next'] = data['Close'].shift(-1) 
 data['Buy'] = np.where( data['Close.next'] > data['Close'], 1, 0 )
 sentiment_data = pd.read_csv('../MarketSentiment/csvData/BA/BA.csv', index_col=0)
@@ -28,14 +28,17 @@ fundamentals_data.index = pd.to_datetime( fundamentals_data.index, format='%Y-%m
 fundamentals_data = fundamentals_data.resample('D').ffill()
 data['Earnings'] = fundamentals_data['eps_diluted']
 data['Book Value'] = fundamentals_data['bvps_diluted']
+data['Earnings.exp'] = np.exp( data['Earnings'] )
+data['Volume.inv'] = 1. / data['Volume']
 data = data.dropna()
-input_vars = ['Close', 'Sentiment', 'Number of Articles', 'Volume', 'Earnings', 'Book Value']
+input_vars = ['Close', 'Close.lag1', 'Close.lag2', 'Close.lag4', 'Close.lag8', 'Sentiment', 'Number of Articles', 'Volume', 'Earnings', 'Book Value', 'Earnings.exp', 'Volume.inv']
 output_var = 'Close.next'
 # Training
 X = data[ input_vars ]
 y = data[ output_var ]
+X = SplineTransformer(degree=3, knots='quantile').fit_transform(X)
 X_train, X_test, y_train, y_test, idx_train, idx_test = train_test_split( X, y, data.index, shuffle=False, train_size=0.7 )
-regr = LinearRegression()
+regr = MLPRegressor( hidden_layer_sizes=[32, 32, 32, 1], activation='relu', solver='lbfgs', alpha=0.00005, max_iter=1000000000, max_fun=1000000, learning_rate='invscaling', learning_rate_init=1e-6 )
 InputScaler = StandardScaler()
 InputScaler.fit( X_train )
 X_train = InputScaler.transform( X_train )
@@ -43,6 +46,7 @@ X_test = InputScaler.transform( X_test )
 X = InputScaler.transform( X )
 regr.fit(X_train, y_train)
 y_hat = regr.predict( X )
+y_hat = np.where( y_hat / data['Close'] > 1.2, data['Close'] * 1.2, y_hat )
 data['Buy.estimate'] = np.where( y_hat > data['Close'], 1, 0 )
 print( accuracy_score( data['Buy'], data['Buy.estimate'] ) )
 print( precision_score( data['Buy'], data['Buy.estimate'] ) )
